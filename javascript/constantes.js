1b06e0f04ef10388bb3571849cedc838d88421f2
Thomast527 <168233515+Thomast527@users.noreply.github.com>
2025-11-29 18:31:54 +0100

    Update constantes.js

--- javascript/constantes.js
+++ javascript/constantes.js
@@ -1,3 +1,4 @@
 "use strict";
 const backendAddress = 'https://Thomast527.pythonanywhere.com/';
+const frontBaseURL = "https://thomast527.github.io/INF1407-Trabalho2-Frontend/";
 const tokenKeyword = 'Token ';
